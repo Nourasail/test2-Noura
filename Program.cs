f7fdcaf819f296573d4cb96f11178f6b4bcf239e
NOURAH SAAD <aliean@outlook.sa>
2024-12-17 17:35:32 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿// using System;
+// using static System.Runtime.InteropServices.JavaScript.JSType;
+
+// class Program
+// {
+// public static void.Mine(string[] areg);
+
+//     //     string name = "Nora";
+//     //     Console.WriteLine("My Name "+name+);
+// }
+
+// Q2
+
+
+// class Program{
+
+// int one = 1;
+// int two = 2;
+
+// Console.WriteLine("number"+one+,+,+two+)
+
+// }
+
+
+
+
+// Q3
+
+// public class Program {
+//     // static void Mine(string[] areg);{
+
+//     public int[] arry = [5];
+
+
+//     for (int arry = 0; arry<length; i++)
+// {
+
+
+// }
+
+//     Console.WriteLine()
+// }
+
+// internal class length
+// {
+// }
+
+
+
+
+// q4
+// class Program
+// {
+//     int n1 = 7;
+//     int n2 = 12;
+
+//     if (true)
+//     {
+//     n1=12
+//     Console.WriteLine(12"is larger")
+
+//     else
+//     {
+//         Console.WriteLine("no")
+//     }
+//     }
+// }
+
+
+// // q5
+// class grade{
+
+
+//     string A = "A";
+//     string B = "B";
+
+//     string C = "C";
+
+//     string D= "D";
+
+//     string F = "F";
+
+
+// switch
+// {
+//     Console.Redlien(A)
+//     Console.WriteLine("Excellent")
+
+//     case;
+//     Console.WriteLine("Very Good")
+
+
+//     default:
+//     Console.WriteLine("Fail")
+
+
+// }
+// }
+
+
+
+
+
+
+//  class Movie 
+// {
+
+//     public static void.private Mine(){
+
+//         int[] = [3]
+
+
+
+//     }
+
+//     private string Title = "Game";
+//     private string Director = "mew";
+//     private double Duration = 199;
+//     Console.WriteLine("Title: {Title}, Director: {Director}, Duration: {Duration} minutes");
+
+
+
+// }
